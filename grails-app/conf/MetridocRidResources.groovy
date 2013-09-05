0cc25bdb71a2691ace310849b3ffa28271dd1ce2
Tommy Barker <mingus.karate@gmail.com>
2013-09-05 13:05:44 -0400

    fix resource issue

--- grails-app/conf/MetridocRidResources.groovy
+++ grails-app/conf/MetridocRidResources.groovy
@@ -3,40 +3,41 @@ modules = {
     tableModule {
         dependsOn "application"
         resource id: 'css',
-                url: [dir: 'css', file: 'pagination.css'],
+                url: [plugin: 'metridocRid', dir: 'css', file: 'pagination.css'],
                 attrs: [type: 'css']
         resource id: 'css',
-                url: [dir: 'css', file: 'table.css'],
+                url: [plugin: 'metridocRid', dir: 'css', file: 'table.css'],
                 attrs: [type: 'css']
         resource id: 'css',
-                url: [dir: 'css', file: 'floating_table.css'],
+                url: [plugin: 'metridocRid', dir: 'css', file: 'floating_table.css'],
                 attrs: [type: 'css']
     }
     datePicker {
         resource id: 'css',
-                url: [dir: 'datepicker/css', file: 'datepicker.css'],
+                url: [plugin: 'metridocRid', dir: 'datepicker/css', file: 'datepicker.css'],
                 attrs: [type: 'css']
+
         resource id: 'js',
-                url: [dir: 'datepicker/js', file: 'bootstrap-datepicker.js'],
+                url: [plugin: 'metridocRid', dir: 'datepicker/js', file: 'bootstrap-datepicker.js'],
                 attrs: [type: 'js']
     }
 
     ridTransaction {
         dependsOn "application"
         resource id: 'css',
-                url: [dir: 'css', file: 'RidTransaction.css'],
+                url: [plugin: 'metridocRid', dir: 'css', file: 'RidTransaction.css'],
                 attrs: [type: 'css']
         resource id: 'js',
-                url: [dir: 'js', file: 'RidTransaction.js'],
+                url: [plugin: 'metridocRid', dir: 'js', file: 'RidTransaction.js'],
                 attrs: [type: 'js']
     }
 
     statistics {
         resource id: 'js',
-                url: [dir: 'js', file: 'RidStatistics.js'],
+                url: [plugin: 'metridocRid', dir: 'js', file: 'RidStatistics.js'],
                 attrs: [type: 'js']
         resource id: 'js',
-                url: [dir: 'js', file: 'RidStatisticsGraph.js'],
+                url: [plugin: 'metridocRid', dir: 'js', file: 'RidStatisticsGraph.js'],
                 attrs: [type: 'js']
     }
 
